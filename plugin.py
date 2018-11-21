419e960ccaf9aeabcb47d2c90ffcd9098cc2c9ac
Kei Iwasaki <me@laughk.org>
2018-11-22 08:12:23 +0900

    Let's make simple weather information plugin 🌄

--- plugin.py
+++ plugin.py
@@ -1,3 +1,6 @@
+import json
+import requests
+from slackbot import settings
 from slackbot.bot import respond_to, listen_to
 
 from models import Memo
@@ -27,3 +30,19 @@ def memo_show(message, name):
         message.reply(f'memo "{name}" is below\n```\n{memo.text}\n```\n')
     else:
         message.reply(f'memo "{name}" ... hmm ..., I don\'t know ¯\\_(ツ)_/¯')
+
+
+@respond_to(r'weather\s+(\S+)')
+def show_weather(message, city):
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    result = requests.get(
+        f'{url}?q={city}&appid={settings.OPENWEATHERMAP_APPID}'
+    )
+    data_dict = json.loads(result.content.decode())
+    if result.status_code == 200:
+        message.reply(
+            '\nCurrent Weather\n'
+            f'{data_dict["name"]}: {data_dict["weather"][0]["description"]}'
+        )
+    else:
+        message.reply('Sorry, I could not get weather Information :sob:')
